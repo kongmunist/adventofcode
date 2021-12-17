89008a40b5ba7dc76b73fdf4362275e939b35442
Andy Kong <andyking99@gmail.com>
2021-12-17 02:10:24 -0600

    Update day4.py

--- day4/day4.py
+++ day4/day4.py
@@ -41,3 +41,102 @@ with open('input.txt', 'r') as f:
 import numpy as np
 # convert boards to numpy arrays
 boards = np.array(boards)
+
+# Done with reading in input.txt
+# Now, let's find the winning board
+
+# First, make a function that checks if a numpy array board has a complete row or column
+def check_row_or_column(board):
+    # check if any row or column is complete
+    for i in range(5):
+        # check if any row is complete
+        if np.all(board[i,:]):
+            return True
+        # check if any column is complete
+        if np.all(board[:,i]):
+            return True
+    return False
+
+# Function that checks if any board is complete, and returns the index of the winning board
+def check_complete(boards):
+    for i in range(len(boards)):
+        if check_row_or_column(boards[i]):
+            return i
+    return None
+
+
+# make corresponding numpy arrays of falses matching the shape of each board
+complete_boards = np.zeros(boards.shape, dtype=bool)
+
+# now, loop through the bingo numbers, and mark off boards that have them
+for number in bingo_numbers:
+    # mark off the boards that have the number
+    complete_boards[boards == number] = True
+
+    # Now check if any board is complete
+    winning_board = check_complete(complete_boards)
+    if winning_board is not None:
+        # if so, print the score
+        # The score is the sum of all unmarked numbers on that board, times the number that was just called
+        # unmarked numbers are numbers where complete_boards[i,j] == False
+        score = np.sum(boards[winning_board][complete_boards[winning_board] == False]) * number
+        print(score)
+        break
+
+########################################################################################################################
+# Part 2
+# On the other hand, it might be wise to try a different strategy: let the giant squid win.
+# You aren't sure how many bingo boards a giant squid could play at once, so rather than waste time counting its arms, the safe thing to do is to figure out which board will win last and choose that one. That way, no matter which boards it picks, it will win for sure.
+# In the above example, the second board is the last to win, which happens after 13 is eventually called and its middle column is completely marked. If you were to keep playing until this point, the second board would have a sum of unmarked numbers equal to 148 for a final score of 148 * 13 = 1924.
+
+# Figure out which board will win last. Once it wins, what would its final score be?
+
+# Solution
+# The solution is the same as before, except now you're looking for the last board to win.
+
+# New check_complete function that takes in a list of indices to ignore
+def check_complete2(boards, ignore_indices):
+    for i in range(len(boards)):
+        if i in ignore_indices:
+            continue
+        if check_row_or_column(boards[i]):
+            return i
+
+
+# Make corresponding numpy arrays of falses matching the shape of each board
+complete_boards = np.zeros(boards.shape, dtype=bool)
+
+# now, loop through the bingo numbers, and mark off boards that have that number, and remove completed boards from boards
+# keep track of boards that won
+winning_boards = []
+# track last number called
+last_number = None
+
+for number in bingo_numbers:
+    # mark off the boards that have the number
+    complete_boards[boards == number] = True
+
+    # Now check if any board is complete
+    winning_board = check_complete2(complete_boards, winning_boards)
+    while winning_board is not None:
+        # if so, add it to the list of winning boards
+        winning_boards.append(winning_board)
+
+        # Now check if any board is complete
+        winning_board = check_complete2(complete_boards, winning_boards)
+
+    # if all boards are complete, break
+    if len(winning_boards) == len(boards):
+        last_number = number
+        break
+
+
+print(winning_boards)
+
+# The last board to win is the last one in the list
+winning_board = winning_boards[-1]
+# calcualte the score of that board
+score = np.sum(boards[winning_board][complete_boards[winning_board] == False]) * last_number
+print(score)
+
+
